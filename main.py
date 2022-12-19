c8e0650bbdfd2258e3e120d5bfb725ea9e351210
Darius Ghomashchian <50425882+Darius0852@users.noreply.github.com>
2022-12-19 22:38:49 +0000

    new

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-import ast
+import pandas as pd
+# import ast
 
-var = input("Enter you're process variable: ")
+# var = input("Enter you're process variable: ")
+
+with open('Example Input - ProcessVariable.txt', 'r') as file:
+    var = file.read().replace('\n', '')
 print("Your variable is " + var)
 print("Lets Convert it!")
 
 # Convert Process Variable
 
-my_list = ast.literal_eval(var)
-print(my_list)
+# my_list = ast.literal_eval(var)
+# df = pd.json_normalize(var)
+# print(df)
+
+splitVar = var.split(']')
+
+for x in splitVar:
+    print("EQUALS " + x)
+
+# print("SplitVar is " + splitVar)
+
+
+# Example Data: [[id:3, name:"John"]]
+# Example Data: [{"A"}, {"B"}]
